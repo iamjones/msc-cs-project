2f360c3f22e3be4c7b8cffbe8e14600c8ee929d9
David Jones <davidjones@Davids-MacBook-Pro.local>
2017-09-11 20:42:02 +0100

    Added a script to generate the MAE

--- src/main/java/aspectdiscovery/mapper/AspectDiscoverySortMapper.java
+++ src/main/java/aspectdiscovery/mapper/AspectDiscoverySortMapper.java
@@ -8,10 +8,19 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 /**
- *
+ * Sorts the aspect words by the number of occurrences in the corpus.
  */
 public class AspectDiscoverySortMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
 
+    /**
+     * Maps each word and number of occurrence value to the context.
+     * The number of occurrences will be the key so in the merge sort phase
+     * of Hadoop the words will be ordered by the number of occurrences.
+     *
+     * @param key LongWritable
+     * @param value Text
+     * @param context Context
+     */
     @Override
     protected void map(LongWritable key, Text value, Context context) {
 
@@ -21,10 +30,7 @@ public class AspectDiscoverySortMapper extends Mapper<LongWritable, Text, IntWri
             context.write(new IntWritable(Integer.parseInt(parts[1].trim())), new Text(parts[0].trim()));
 
         } catch (IOException | InterruptedException e) {
-            // @TODO - log this to somewhere useful
             System.out.println(e.getMessage());
         }
     }
 }
-
-
